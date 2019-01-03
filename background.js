8470ca698c53072adc0325123ee6527ddb411737
Balvin Perrie <balvin.perrie@gmail.com>
2019-01-03 09:23:22 +0100

    version 0.1.1

--- background.js
+++ background.js
@@ -27,21 +27,37 @@ var permission = links => {
   }));
 };
 
-var onClicked = link => chrome.storage.local.get({
-  width: 700,
-  height: 500,
-  left: screen.availLeft + Math.round((screen.availWidth - 700) / 2),
-  top: screen.availTop + Math.round((screen.availHeight - 500) / 2)
-}, prefs => {
-  chrome.windows.create({
-    url: chrome.extension.getURL('data/window/index.html') + (link ? '?link=' + link : ''),
-    width: prefs.width,
-    height: prefs.height,
-    left: prefs.left,
-    top: prefs.top,
-    type: 'popup'
-  });
-});
+var onClicked = link => {
+  if (ports.length) {
+    if (link) {
+      ports[0].postMessage({
+        method: 'convert',
+        link
+      });
+    }
+    chrome.windows.update(ports[0].sender.tab.windowId, {
+      focused: true
+    });
+  }
+  else {
+    chrome.storage.local.get({
+      width: 750,
+      height: 500,
+      left: screen.availLeft + Math.round((screen.availWidth - 700) / 2),
+      top: screen.availTop + Math.round((screen.availHeight - 500) / 2)
+    }, prefs => {
+      chrome.windows.create({
+        url: chrome.extension.getURL('data/window/index.html') + (link ? '?link=' + link : ''),
+        width: prefs.width,
+        height: prefs.height,
+        left: prefs.left,
+        top: prefs.top,
+        type: 'popup'
+      });
+    });
+  }
+};
+
 chrome.browserAction.onClicked.addListener(() => onClicked());
 
 {
@@ -68,19 +84,32 @@ chrome.contextMenus.onClicked.addListener(info => {
   if (info.menuItemId === 'convert-media' || info.menuItemId === 'convert-link') {
     const link = info.menuItemId === 'convert-media' ? info.srcUrl : info.linkUrl;
 
-    permission([link]).then(() => {
-      if (ports.length) {
-        ports[0].postMessage({
-          method: 'convert',
-          link
-        });
-        chrome.windows.update(ports[0].sender.tab.windowId, {
-          focused: true
-        });
-      }
-      else {
-        onClicked(link);
-      }
-    });
+    permission([link]).then(() => onClicked(link));
   }
 });
+
+{
+  const {onInstalled, setUninstallURL, getManifest} = chrome.runtime;
+  const {name, version} = getManifest();
+  const page = getManifest().homepage_url;
+  onInstalled.addListener(({reason, previousVersion}) => {
+    chrome.storage.local.get({
+      'faqs': true,
+      'last-update': 0
+    }, prefs => {
+      if (reason === 'install' || (prefs.faqs && reason === 'update')) {
+        const doUpdate = (Date.now() - prefs['last-update']) / 1000 / 60 / 60 / 24 > 45;
+        if (doUpdate && previousVersion !== version) {
+          chrome.tabs.create({
+            url: page + '?version=' + version +
+              (previousVersion ? '&p=' + previousVersion : '') +
+              '&type=' + reason,
+            active: reason === 'install'
+          });
+          chrome.storage.local.set({'last-update': Date.now()});
+        }
+      }
+    });
+  });
+  setUninstallURL(page + '?rd=feedback&name=' + encodeURIComponent(name) + '&version=' + version);
+}
